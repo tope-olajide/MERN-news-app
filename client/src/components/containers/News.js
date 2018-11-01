513e1f95b401c0d9c3f5e001fd8b5e21693a7d6f
Temitope <tope_olajide@outlook.com>
2018-11-01 16:54:29 +0100

    Modified all container component

--- client/src/components/containers/News.js
+++ client/src/components/containers/News.js
@@ -1,42 +1,38 @@
 import React, { Component} from 'react';
 import NewsItemListing from '../presentation/NewsItemListing';
+
 import { connect } from 'react-redux'
+import { fetchNews } from '../../actions/newsActions'
 
 class News extends Component {
-    componentDidMount(){
 
-        var fakeNews = [{
-            id: '1',
-            title: 'Mad owl chases car',
-            teaser: 'Mad owl seen tormenting drivers in Morecambe'
-        }, {
-            id: '2',
-            title: 'Owl stowaway',
-            teaser: 'Despicable owl impersonates passenger to board flight to Luton'
-        }];
-        
-        this.props.dispatch(fetchNews(fakeNews));
-    }
-    render(){
 
-        const newsItems = this.props.news.map( (news, i) => {
-            return ( <li key={i}><NewsItemListing data = {news} /></li> );
-        });
+	componentDidMount(){
+		this.props.dispatch(fetchNews());
+	}
 
-        return (
-            <div>
-                <h2>News Items</h2>
-                {(this.props.news.length > 0) ? <ul>{newsItems}</ul> : <div>Sorry we have no news</div>}
-            
-            </div>
-        )
-    }
+
+	render(){
+
+		const newsItems = this.props.news.map( (news, i) => {
+			return ( <li key={i}><NewsItemListing data = {news} /></li> );
+		});
+
+		return (
+			<div>
+				<h2>News Items</h2>
+				<ul>
+					{(this.props.news.length > 0) ? <ul>{newsItems}</ul> : <div>Sorry we have no news</div>}
+				</ul>
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = state => {
-    return {
-        news: state.news.news
-    }
+	return {
+    	news: state.news.news
+	}
 }
 
 export default connect(mapStateToProps)(News)
--- client/src/components/containers/NewsArticle.js
+++ client/src/components/containers/NewsArticle.js
@@ -1,44 +1,35 @@
 import React, { Component} from 'react';
 import NewsItemDetail from '../presentation/NewsItemDetail';
+import CommentsPanel from './CommentsPanel';
 import { connect } from 'react-redux'
-import { fetchNewsItem } from '../../actions/actions'
+import { fetchNewsItem } from '../../actions/newsActions'
 
 class NewsArticle extends Component {
 
 
-    componentDidMount(){
+	componentDidMount(){
+		this.props.dispatch(fetchNewsItem(this.props.match.params.id));
+	}
 
-        var fakeNewsItem = {
-            id: '1',
-            title: 'Mad owl chases car',
-            teaser: 'Mad owl seen tormenting drivers in Morecambe',
-            body: `Morecambe - Tuesday 8th August 2017
+	render(){
 
-            Yesterday evening motorists were left running for their lives as a mad owl began a campaign of terror on rush traffic. 
-            Eye Witness, Eric Barnes said "When I heard it Squawk in the sky above me, I thought I was done for"`
-        };
-        
-        this.props.dispatch(fetchNewsItem(fakeNewsItem));
-    }
-
-    render(){
-        let { newsItem } = this.props; 
-
-        return (
-            <div>
-                <h2>News Story</h2>
-                <ul>
-                    { newsItem ? <NewsItemDetail data={newsItem} /> : null}
-                </ul>
-            </div>
-        )
-    }
+		return (
+			<div>
+				<h2>News Story</h2>
+				<ul>
+					{ !this.props.newsItemLoading ? <div><NewsItemDetail data={this.props.newsItem} /> <CommentsPanel comments={this.props.comments} id={this.props.newsItem._id} /></div> : <div>Loading</div>}
+				</ul>
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = state => {
-    return {
-        newsItem: state.news.newsItem
-    }
+	return {
+    	newsItem: state.news.newsItem,
+    	comments: state.news.newsItem.comments,
+    	newsItemLoading: state.news.newsItemLoading
+	}
 }
 
 export default connect(mapStateToProps)(NewsArticle)

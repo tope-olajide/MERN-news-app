cbf50da434ce074ea393db7cdab9a412ac07abdd
Temitope <tope_olajide@outlook.com>
2018-11-01 16:53:06 +0100

    Modified all presentation component

--- client/src/components/presentation/NewsItemDetail.js
+++ client/src/components/presentation/NewsItemDetail.js
@@ -1,24 +1,24 @@
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
+import { fetchNewsItem } from '../../actions/newsActions'
 
 class NewsItemDetail extends Component {
 
-    render(){
-        return (
-            <div>
-                <h2>{this.props.data.title}</h2>
-                <p>{this.props.data.body}</p>
-            </div>
-        )
-    }
+	render(){
+		return (
+			<div>
+				<h2>{this.props.data.title}</h2>
+				<p>{this.props.data.body}</p>
+			</div>
+		)
+	}
 }
 
 NewsItemDetail.propTypes = {
-
     data: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        body: PropTypes.string.isRequired
+		_id: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		body: PropTypes.string.isRequired
     })
 };
 
--- client/src/components/presentation/NewsItemListing.js
+++ client/src/components/presentation/NewsItemListing.js
@@ -1,24 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component} from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class NewsItemListing extends Component {
-    render() {
-        return (
-        <div>
-            <div><Link to={`/news/${this.props.data.id}`}><b>{this.props.data.title}</b></Link></div>
-            <div>{this.props.data.teaser}</div>
-        </div>
-
-        )
-    }
+	render(){
+		return (
+			<div>
+				<div><Link to={`/news/${this.props.data._id}`}><b>{this.props.data.title}</b></Link></div>
+				<div>{this.props.data.teaser}</div>
+			</div>
+		)
+	}
 }
+
 
 NewsItemListing.propTypes = {
     data: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        teaser: PropTypes.string.isRequired
-})
+		_id: PropTypes.string.isRequired,
+		title: PropTypes.string.isRequired,
+		teaser: PropTypes.string.isRequired
+    })
 };
 
 export default NewsItemListing

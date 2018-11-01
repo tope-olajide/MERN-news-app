d09bc3630339bd3a66acae41d5e70c8be75862ad
Temitope <tope_olajide@outlook.com>
2018-11-01 16:51:32 +0100

    Modfied all compoent in layouts

--- client/src/components/layouts/Home.js
+++ client/src/components/layouts/Home.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component} from 'react';
 import News from '../containers/News';
 
 class Home extends Component {
-    render() {
-        return ( 
-            <div>
-        <div> Welcome to MERN News App </div>
-                    <div> 
-                <News />
-            </div> </div>
-        )
-    }
+	render(){
+		return (
+			<div>
+				<div>Welcome to Owl News</div>
+
+				<div>
+					<News />
+				</div>
+			</div>
+		)
+	}
 }
 
-export default Home;
+export default Home

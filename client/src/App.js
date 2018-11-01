6fadfaa346e8d726836792e33ee80dd610d90c3a
Temitope <tope_olajide@outlook.com>
2018-11-01 12:00:30 +0100

    Create NewsItemDetails component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,16 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import Home from './components/layouts/Home';
 import About from './components/layouts/About';
 import Layout from './components/layouts/Layout';
- 
+import NewsItemDetail from './components/presentation/NewsItemDetail';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <Layout>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-      </Layout>
+            <Layout>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path='/news/:id' component={NewsItemDetail}/>    
+            </Layout>
   </BrowserRouter>
        );
   }
---
+++ client/src/components/presentation/NewsItemDetail.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+
+class NewsItemDetail extends Component {
+    render(){
+        return (
+            <div>
+                <h2>News story title</h2>
+                <p>Body: test mad owl story text </p>
+            </div>
+        )
+    }
+}
+
+export default NewsItemDetail
